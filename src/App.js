f931fcdf8a966f502737d0c5eba48817fe59f7bc
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-21 23:51:17 +0000

    navbar redesigned

--- src/App.js
+++ src/App.js
@@ -1,75 +1,9 @@
-import React, { Component } from "react";
-import "./App.scss";
-import About from "./components/About";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faFacebook,
-  faFreeCodeCamp,
-  faCodepen
-} from "@fortawesome/free-brands-svg-icons";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import React from "react";
+import Nav from "./components/Nav";
+
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="navbar">
-          <div>el</div>
-          <ul className="nav-list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">portfolio</a>
-            </li>
-            <li>
-              <a href="#">Apply</a>
-            </li>
-          </ul>
-        </div>
-        <div className="content">
-          <div className="main-area">
-            <section id="home">
-              <p id="p1">Hi, my name is </p>
-              <h1>Yakubu A. El-Rufai</h1>
-              <h1 id="h2">I build things for the web</h1>
-              <p id="p2">
-                I'm a software engineer based in Boston, MA specializing in
-                developing (and occasionally designing) exceptional,
-                high-quality websites and applications. Currently working at
-                Upstatement as an Engineer.
-              </p>
-              <button className="contactButton">Get in Touch</button>
-            </section>
-            <About />
-          </div>
-          <div className="right-section">
-            <div className="email">
-              <p>yrufai23@gmail.com</p>
-            </div>
-          </div>
-          <div className="left-section">
-            <ul>
-              <li>
-                <FontAwesomeIcon icon={faGithub} color="white" />
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faFacebook} color="white" />
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faFreeCodeCamp} color="white" />
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faCodepen} color="white" />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
+    return <Nav />;
   }
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,34 @@
 import React from "react";
+import "./Nav.scss";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div className="container-fluid" />;
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="brand">El-rufai</div>
+        <ul className="navlist">
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Skills</a>
+          </li>
+          <li>
+            <a>Work</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="burger">
+        <div className="class1" />
+        <div className="class2" />
+        <div className="class3" />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
