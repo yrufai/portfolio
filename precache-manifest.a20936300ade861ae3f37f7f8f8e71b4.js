a1070f0d92d2a23961ec5e202be99f1911a44e71
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-22 23:39:10 +0000

    Updates

---
+++ precache-manifest.a20936300ade861ae3f37f7f8f8e71b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "64a540c12aac15bd176a",
+    "url": "/portfolio/static/js/main.64a540c1.chunk.js"
+  },
+  {
+    "revision": "66f2cde51455fecae2eb",
+    "url": "/portfolio/static/js/2.66f2cde5.chunk.js"
+  },
+  {
+    "revision": "64a540c12aac15bd176a",
+    "url": "/portfolio/static/css/main.ea77ed05.chunk.css"
+  },
+  {
+    "revision": "abfdefb560103291c90dc257d96ddb5e",
+    "url": "/portfolio/index.html"
+  }
+];
