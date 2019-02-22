498d0da9425f17f8f1269d211355c406f0fc4c1e
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-22 03:43:26 +0000

    navbar done

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Nav.scss";
-import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
@@ -9,20 +8,38 @@ const Nav = () => {
         <div className="brand">El-rufai</div>
         <ul className="navlist">
           <li>
-            <a>About</a>
+            <a href="#">About</a>
           </li>
           <li>
-            <a>Skills</a>
+            <a href="#">Skills</a>
           </li>
           <li>
-            <a>Work</a>
+            <a href="#">Work</a>
           </li>
           <li>
-            <a>Contact</a>
+            <a href="#">Contact</a>
           </li>
         </ul>
       </div>
-      <div className="burger">
+      <div
+        className="burger"
+        onClick={() => {
+          const nave = document.querySelector(".navlist");
+          const toggle = document.querySelector(".burger");
+          toggle.classList.toggle("toggle");
+          const links = document.querySelectorAll(".navlist li");
+          nave.classList.toggle("active-nav");
+          links.forEach((links, index) => {
+            if (links.style.animation) {
+              links.style.animation = "";
+            } else {
+              links.style.animation = `anim-navlinks 0.4s ease forwards ${index /
+                4 +
+                1.5}s`;
+            }
+          });
+        }}
+      >
         <div className="class1" />
         <div className="class2" />
         <div className="class3" />
