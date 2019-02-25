92d921184495690b839a0c5e8b1378d96ca486b4
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-25 20:29:06 +0000

    Updates

---
+++ precache-manifest.bb1c248657eaed5d940f1a3b5edca1ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "e7bf154c12aa707460c1",
+    "url": "/portfolio/static/js/main.e7bf154c.chunk.js"
+  },
+  {
+    "revision": "3146bbf2513183549532",
+    "url": "/portfolio/static/js/2.3146bbf2.chunk.js"
+  },
+  {
+    "revision": "e7bf154c12aa707460c1",
+    "url": "/portfolio/static/css/main.8820ab31.chunk.css"
+  },
+  {
+    "revision": "5d210a6b5c2513a91bf2188859914e06",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3a7fa7e4e63cc0dfdbaa69ac5e85a0a1.js"
+  "/portfolio/precache-manifest.bb1c248657eaed5d940f1a3b5edca1ca.js"
 );
 
 workbox.clientsClaim();
