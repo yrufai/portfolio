5c047850ff025fb2f85ebf325a3dbc7cd5eabf99
yakubu rufai mahatma <yrufai23@gmail.com>
2019-04-13 19:52:07 +0000

    editeed

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,28 +10,9 @@ class Home extends React.Component {
     super(props);
     //this.display = this.display.bind(this);
     this.state = {
-      name: "I'm Yakubu Ahd El-rufai"
+      name: "I'm Yakubu Ahmed El-rufai"
     };
   }
-
-  /*componentWillMount() {
-    this.display();
-  }
-
-  /*display() {
-    let index = 0;
-    const getDem = setInterval(() => {
-      let myString = this.state.name.charAt(index);
-      document.getElementById("name").innerHTML += myString;
-      let calor = document.getElementById("name");
-      calor.style.color = "#d639bc";
-      index++;
-      if (index === this.state.name.length) {
-        clearInterval(getDem);
-        calor.style.color = "#8892b0";
-      }
-    }, 300);
-  }*/
   render() {
     return (
       <div className="home" id="homepage">
